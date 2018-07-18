19dc32dd51903369033991cd665c5847d7cc261c
Dibyo Majumder <dibyomajumder23@gmail.com>
2018-07-19 04:00:22 +0530

    Included profile route by user_id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -70,6 +70,28 @@ router.get("/handle/:handle", (req, res) => {
     });
 });
 
+// @route   GET api/profile/user/:user_id          // Its a backend api route it gets hit by the frontend
+// @desc    Get Profile by User ID
+// @access  Public
+
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+  Profile.findOne({
+    user: req.params.user_id
+  })
+    .populate("user", ["name", "avatar "])
+    .then(profile => {
+      if (!profile) {
+        errors.noProfile = "There is no profile for this user ";
+        res.status(400).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => {
+      res.status(404).json(err);
+    });
+});
+
 // @route   POST api/profile
 // @desc    Create or Update current users profile
 // @access  Private
