54bffccbc61d75af08bca6e4e97caf1b9c017b36
Dibyo Majumder <dibyomajumder23@gmail.com>
2018-07-18 10:33:00 +0530

    @private route for profile create and update is made

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -22,7 +22,7 @@ router.get("/test", (req, res) => {
 });
 
 // @route   GET api/profile
-// @desc    Get current users profile
+// @desc    Get current users' profile
 // @access  Private
 
 router.get(
@@ -40,6 +40,69 @@ router.get(
         res.json(profile);
       })
       .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route   POST api/profile
+// @desc    Create or Update current users profile
+// @access  Private
+
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    // Get Feilds
+    const profileFeilds = {};
+
+    profileFeilds.user = req.user.id;
+    if (req.body.handle) profileFeilds.handle = req.body.handle;
+    if (req.body.company) profileFeilds.company = req.body.company;
+    if (req.body.website) profileFeilds.website = req.body.website;
+    if (req.body.location) profileFeilds.location = req.body.location;
+    if (req.body.status) profileFeilds.status = req.body.status;
+    if (req.body.bio) profileFeilds.bio = req.body.bio;
+    if (req.body.githubusername)
+      profileFeilds.githubusername = req.body.githubusername;
+
+    // Skills - split into array
+    if (typeof req.body.skills !== "undefined") {
+      profileFeilds.skills = req.body.skills.split(",");
+    }
+
+    // Social - initialize first
+    profileFeilds.social = {};
+
+    if (req.body.youtube) profileFeilds.social.youtube = req.body.youtube;
+    if (req.body.twitter) profileFeilds.social.twitter = req.body.twitter;
+    if (req.body.facebook) profileFeilds.social.facebook = req.body.facebook;
+    if (req.body.instagram) profileFeilds.social.instagram = req.body.instagram;
+    if (req.body.linkedin) profileFeilds.social.handle = req.body.handle;
+
+    // Check if user is present then update else create a new one
+
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      if (profile) {
+        // Update
+        Profile.findByIdAndUpdate(
+          { user: req.user.id },
+          { $set: profileFeilds },
+          { new: true }
+        ).then(profile => res.json(profile));
+      } else {
+        //Create
+
+        // Check if handle exists
+        Profile.findOne({ handle: req.user.handle }).then(profile => {
+          if (profile) {
+            errors.handle = "Handle already exists";
+            res.status(400).json(errors);
+          }
+
+          // Save profile
+          new Profile(profileFeilds).save().then(profile => res.json(profile));
+        });
+      }
+    });
   }
 );
 
