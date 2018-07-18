c8f77db03b1e807a4e90aeef851fe108c9bf04b1
Dibyo Majumder <dibyomajumder23@gmail.com>
2018-07-18 09:46:36 +0530

    current user profile route setup done

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,15 @@
 // WIll have things like bio, location, and other descriptions
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const passport = require("passport");
+
+// Load Profile Model
+const Profile = require("../../models/Profile");
+
+// Load User Model
+const User = require("../../models/User");
 
 // @route   GET api/profile/test
 // @desc    Tests Posts Route
@@ -11,5 +20,27 @@ router.get("/test", (req, res) => {
     msg: "User Profile Works"
   });
 });
+
+// @route   GET api/profile
+// @desc    Get current users profile
+// @access  Private
+
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noProfile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
