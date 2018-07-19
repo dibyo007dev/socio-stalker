5ca9c2a23597ffc2d49c67907b411cb15a93d2d9
Dibyo Majumder <dibyomajumder23@gmail.com>
2018-07-19 18:40:11 +0530

    get all posts route added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -24,6 +24,17 @@ router.get("/test", (req, res) => {
   });
 });
 
+// @route   GET api/posts
+// @desc    Fetch posts
+// @access  Public
+
+router.get("/", (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404).json(err));
+});
+
 // @route   POST api/posts
 // @desc    Create Posts
 // @access  Private
