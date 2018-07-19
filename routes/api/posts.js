6d84121a1ffc9ae304ab7d273892e9f02e451f78
Dibyo Majumder <dibyomajumder23@gmail.com>
2018-07-19 21:02:41 +0530

    Like and dislike routes included

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -9,6 +9,9 @@ const passport = require("passport");
 // Load Post Model
 const Post = require("../../models/Post");
 
+// Load Profile Model
+const Profile = require("../../models/Profile");
+
 //------------------ Load Validation ------------------//
 const validatePostInput = require("../../validation/post");
 
@@ -67,6 +70,104 @@ router.post(
       user: req.user.id
     });
     newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route   DELETE api/posts/:id
+// @desc    Delete posts by ID
+// @access  Private
+
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id).then(post => {
+        // Check for Post owner
+        if (post.user.toString() !== req.user.id) {
+          return res.status(401).json({ notAuthorised: "User not authorised" });
+        }
+        // Delete
+
+        Post.remove()
+          .then(() => res.json({ success: true }))
+          .catch(err =>
+            res.status(404).json({ postNotFound: "No Post with this id" })
+          );
+      });
+    });
+  }
+);
+
+// @route   POST api/posts/like/:id
+// @desc    Like Post by ID
+// @access  Private
+
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // Check if user already liked the post
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            return res
+              .status(400)
+              .json({ alreadyLiked: "User already liked the posts" });
+          }
+
+          // Otherwise add the user in the like array
+
+          post.likes.unshift({
+            user: req.user.id
+          });
+
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json("Post Not Found "));
+    });
+  }
+);
+
+// @route   POST api/posts/unlike/:id
+// @desc    Unlike Post by ID
+// @access  Private
+
+router.post(
+  "/unlike/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          // Check if user already liked the post
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length === 0
+          ) {
+            return res
+              .status(400)
+              .json({ notLiked: "You havn't liked the post yet" });
+          }
+
+          // If liked then dislike the post
+
+          const removeIndex = post.likes
+            .map(item => item.user.toString())
+            .indexOf(req.user.id);
+
+          // Splice it out of the array
+
+          post.likes.splice(removeIndex, 1);
+
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json("Post Not Found "));
+    });
   }
 );
 
