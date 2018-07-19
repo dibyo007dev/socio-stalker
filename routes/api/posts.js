e23bb55966b93831419a384c51f403a45a1edc09
Dibyo Majumder <dibyomajumder23@gmail.com>
2018-07-19 18:09:42 +0530

    Add post route created

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,6 +2,11 @@
 
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+
+const passport = require("passport");
+
+const Post = require("../../models/Post");
 
 // @route   GET api/posts/test
 // @desc    Tests Posts Route
@@ -12,5 +17,23 @@ router.get("/test", (req, res) => {
     msg: "Posts Works"
   });
 });
+
+// @route   POST api/posts
+// @desc    Create Posts
+// @access  Private
+
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+    newPost.save().then(post => res.json(post));
+  }
+);
 
 module.exports = router;
